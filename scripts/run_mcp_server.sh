d2774e847142199157bae336940c217a358d9808
James Laska <1051173+jlaska@users.noreply.github.com>
2025-09-26 12:41:07 -0400

    Minor fixes

--- scripts/run_mcp_server.sh
+++ scripts/run_mcp_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /Users/jlaska/Projects/RedHatLDAP-MCP || exit
-export REDHAT_LDAP_CONFIG="/Users/jlaska/Projects/RedHatLDAP-MCP/config/redhat-ldap.json"
+export REDHAT_LDAP_CONFIG="$PWD/config/redhat-ldap.json"
 exec /opt/homebrew/bin/uv run python -m redhat_ldap_mcp.server "$@"
